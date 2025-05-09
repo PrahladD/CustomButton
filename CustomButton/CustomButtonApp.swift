5922c6a8800d2b315abd7435487b947f774a7b65
Prahlad <prahlad.dhungana@aircanada.ca>
2025-05-09 18:58:07 -0300

    Initial Commit

---
+++ CustomButton/CustomButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonApp.swift
+//  CustomButton
+//
+//  Created by Prahlad Dhungana on 2025-05-09.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
